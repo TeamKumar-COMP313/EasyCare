1b1fa2c66bac132fa1ad7ef8143c63f5f565f09d
Shahbaaz <shahbaaz1491@gmail.com>
2015-02-02 00:35:56 -0500

    Patient Check List

--- EasyCare/gen/com/example/easycare/R.java
+++ EasyCare/gen/com/example/easycare/R.java
@@ -25,39 +25,59 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int ScrlView=0x7f080006;
-        public static final int action_settings=0x7f080010;
+        public static final int RadioButton01=0x7f08000a;
+        public static final int RadioButton02=0x7f080009;
+        public static final int RadioButton03=0x7f08000b;
+        public static final int RadioButton04=0x7f080018;
+        public static final int RadioButton05=0x7f08001a;
+        public static final int RadioButton06=0x7f080019;
+        public static final int RadioButton07=0x7f080010;
+        public static final int RadioButton08=0x7f08000f;
+        public static final int RadioButton09=0x7f080011;
+        public static final int RadioGroup01=0x7f080006;
+        public static final int RadioGroup02=0x7f08000e;
+        public static final int RadioGroup03=0x7f08000c;
+        public static final int ScrlView=0x7f08001d;
+        public static final int action_settings=0x7f080021;
         public static final int button1=0x7f080004;
         public static final int button2=0x7f080005;
         public static final int editText1=0x7f080001;
         public static final int editText2=0x7f080003;
-        public static final int editText3=0x7f08000c;
-        public static final int editText4=0x7f08000e;
-        public static final int radio0=0x7f080008;
-        public static final int radio1=0x7f080009;
+        public static final int editText3=0x7f08001e;
+        public static final int editText4=0x7f08001f;
+        public static final int radio0=0x7f080012;
+        public static final int radio1=0x7f080013;
+        public static final int radio2=0x7f080014;
         public static final int radioGroup1=0x7f080007;
-        public static final int regbut=0x7f08000f;
+        public static final int regbut=0x7f080020;
         public static final int textView1=0x7f080000;
         public static final int textView2=0x7f080002;
-        public static final int textView3=0x7f08000a;
-        public static final int textView4=0x7f08000b;
+        public static final int textView3=0x7f080008;
+        public static final int textView4=0x7f08001b;
         public static final int textView5=0x7f08000d;
+        public static final int textView6=0x7f080016;
+        public static final int textView7=0x7f080015;
+        public static final int textView8=0x7f080017;
+        public static final int textView9=0x7f08001c;
     }
     public static final class layout {
         public static final int activity_login=0x7f030000;
         public static final int activity_main=0x7f030001;
-        public static final int activity_regiter=0x7f030002;
+        public static final int activity_patient__check_list=0x7f030002;
+        public static final int activity_regiter=0x7f030003;
     }
     public static final class menu {
         public static final int login=0x7f070000;
         public static final int main=0x7f070001;
-        public static final int regiter=0x7f070002;
+        public static final int patient__check_list=0x7f070002;
+        public static final int regiter=0x7f070003;
     }
     public static final class string {
         public static final int action_settings=0x7f050002;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050001;
         public static final int title_activity_login=0x7f050004;
+        public static final int title_activity_patient__check_list=0x7f050005;
         public static final int title_activity_regiter=0x7f050003;
     }
     public static final class style {
